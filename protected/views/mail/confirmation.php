8f9e2edca7de9dd7ac8607bc342ef28a53cda48a
Fidel Montesino <fmontesino@asmws.com>
2014-02-12 18:11:37 +0100

    Email to confirm password

---
+++ protected/views/mail/confirmation.php
@@ -0,0 +1,9 @@
+<p>Hello <?php print $email ?>,</p>
+
+<p>Welcome to HUAXIN. Before you can log in and publish some ads, you must confirm your email.<br>
+	To do so, please click on this link: <a href="<?php echo $link ?>"><?php echo $link ?></a> or copy and paste it on your browser.
+</p>
+
+<p>Thanks for trusting us.</p>
+
+<p>The HUAXIN Team</p>

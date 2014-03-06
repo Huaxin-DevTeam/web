c13585cb72366f98838098586cd4dad7528bb585
iris <iris.serranoc@gmail.com>
2014-03-06 12:30:35 +0100

    lazo y sombra creditos

--- protected/views/item/credit_option.php
+++ protected/views/item/credit_option.php
@@ -1,13 +1,15 @@
 <li class="list-unstyled creditos-lista">
 
 	<a href='order/cart/<?php print $item->id ?>'>
-		<div class="credit_wrapper text-center col-xs-12 col-sm-6">
-			<div class="credit_name"><?php print $item->name ?></div>
-			<div class="credit_num">n creditos</div>
-			<div class="credit_desc">	
-				<div class="credit_euros">3euroo</div>
-				<div class="credit_phrase">soy el ole de golem </div>
-				<div class="buy_now center-block">buy now</div>
+		<div class="text-center col-xs-12 col-sm-6">
+			<div class="credit_wrapper">
+				<div class="credit_name"><?php print $item->name ?></div>
+				<div class="credit_num">n creditos</div>
+				<div class="credit_desc">	
+					<div class="credit_euros">3euro</div>
+					<div class="credit_phrase">soy el ole </div>
+					<div class="buy_now center-block">buy now</div>
+				</div>
 			</div>
 		</div>
 

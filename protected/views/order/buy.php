317dd33fd0ad98f4e8f1b71ddc01e6f27d0ee84a
iris <iris.serranoc@gmail.com>
2014-03-18 17:05:18 +0100

    caca

--- protected/views/order/buy.php
+++ protected/views/order/buy.php
@@ -1,2 +1,21 @@
-<a href="/order/confirm/<?php print $token ?>/<?php print $payerId ?>" class="button">Go!</a>
-<a href="/order/cancel/<?php print $token ?>" class="button">Cancel</a>
+
+<div class="pagar">
+<span class="col-xs-12 margenh5">Ya casi hemos terminado, revisa y confirma tu compra</span>
+
+	<div class="resumen-compra col-xs-12">
+		<table class="table col-xs-12">
+			<tr>
+				<th class="col-xs-6">Resumen de compra:</th>
+				<th class="col-xs-6">Precio</th>
+			</tr>
+			<tr>
+				<td class="col-xs-6">un puñau de créditos.</td>
+				<td class="col-xs-6">45€</td>
+			</tr>	
+			
+		</table>
+    </div>
+	<a href="/order/confirm/<?php print $token ?>/<?php print $payerId ?>" class="button  button-confirm pull-right text-center bigbottom">Confirmar compra</a>
+	
+</div>
+<a href="/order/cancel/<?php print $token ?>" class="col-xs-5">Cancelar la compra</a>

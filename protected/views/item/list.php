948f13e329f1a3517ada34f8eff9ac3cfdd22547
iris <iris.serranoc@gmail.com>
2014-03-26 11:39:08 +0100

    nuevo tamaño anuncio/imagen

--- protected/views/item/list.php
+++ protected/views/item/list.php
@@ -2,10 +2,10 @@
 <?php print_r($item); ?>
 </pre>-->
 <div class="blueline col-md-12 clearfix">
-	<div class="imagen col-xs-4">
+	<div class="imagen col-xs-2">
 		<img src="<?php echo Yii::app()->request->baseUrl; ?><?php print $item->image_url; ?>" class="img-responsive" />
 	</div>
-	<div class="texto col-xs-8">
+	<div class="texto col-xs-10">
 		<h5><a href="<?php echo Yii::app()->createUrl('view/'.$item->id) ?>">
 			<?php print $item->title; ?>
 		</a></h5>
@@ -13,7 +13,7 @@
 			<?php print $item->description; ?>
 		</div>
 		<div class="row bottom">
-			<div class="col-xs-12">
+			<div class="">
 				<span class="pull-left precio"><?php print $item->price; ?>€</span>
 				<div class="actions pull-right">
 					<ul class="anuncio list-inline list-unstyled clearfix">
@@ -21,7 +21,7 @@
 						<li class="editar"><a href="/edit/<?php print $item->id?>">Editar</a></li>
 						<li class="eliminar"><a href="/delete/<?php print $item->id?>" class="delete confirm">Eliminar</a></li>
 						<?php endif; ?>
-						<li class=""><a href="#" class="button-blue veranuncio">Ver anuncio</a></li>
+						<li class=""><a href="<?php echo Yii::app()->createUrl('view/'.$item->id) ?>" class="button-blue veranuncio">Ver anuncio</a></li>
 					</ul>
 				</div>
 			</div>				

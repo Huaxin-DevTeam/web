454ee0b92415f8487be1892edf81cdc501c3f40b
iris <iris.serranoc@gmail.com>
2014-03-26 13:03:28 +0100

    views footer :$

---
+++ protected/views/site/about.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+/* @var $error array */
+
+$this->pageTitle=Yii::app()->name . ' - Sobre nosotros';
+$this->breadcrumbs=array(
+	'Error',
+);
+?>
+
+<div class="col-xs-12 blue margenh5">
+    <h5>Sobre nosotros</h5>
+</div>
+
+华信信息平台，致力于打造旅行华人社区最权威最全面的商业信息平台。<br/>华信传媒，拥有专业的技术团队和管理团队。我们开发的华信软件为全西班牙华人华侨提供贴心的全面的无处不在的信息服务。助力旅西华人华侨的创业和生活。彻底改变旅西华人社区居住分散，信息不通的弊端。<br/>华信信息软件，分为网站和手机软件。为不同喜好的客户提供最合适的服务，手机软件也可以下载到平板电脑中使用。弹指一按，信息尽现。真正实现足不出户，信息共享互通。<br/>

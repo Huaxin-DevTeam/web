4db48a38b8ae4882a65f931a25a678da7f830165
Fidel Montesino <fmontesino@asmws.com>
2014-04-11 19:00:27 +0200

    update db

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 06-03-2014 a las 17:52:01
+-- Tiempo de generación: 11-04-2014 a las 19:00:07
 -- Versión del servidor: 5.5.29
 -- Versión de PHP: 5.4.10
 
@@ -36,10 +36,10 @@ CREATE TABLE `ad` (
 --
 
 INSERT INTO `ad` (`id`, `image_url`, `link`, `is_mobile`, `num_views`, `date_published`, `date_end`) VALUES
-(1, 'http://www.parajunkee.com/wp-content/uploads/2014/01/ad_160x200.png', 'http://dev.mihx.es', 0, 2489, '2013-12-20 14:43:41', '2015-01-06 23:00:00'),
-(2, 'http://th00.deviantart.net/fs6/200H/i/2005/105/f/e/iSquid_______iPod_parody_ad_by_ashbet.jpg', 'http://dev.mihx.es', 0, 2448, '2013-12-20 14:44:25', '2015-12-29 23:00:00'),
-(3, 'http://www.ecoeastend.com/images/ad%20images/half-ad-1.gif', 'http://dev.mihx.es', 0, 2700, '2013-12-17 23:00:00', '2015-12-18 23:00:00'),
-(4, 'http://farm5.staticflickr.com/4078/4743777048_e8ac9c0c8a.jpg', 'http://dev.mihx.es', 0, 2509, '2013-12-19 23:00:00', '2015-01-09 23:00:00');
+(1, 'http://www.parajunkee.com/wp-content/uploads/2014/01/ad_160x200.png', 'http://dev.mihx.es', 0, 3088, '2013-12-20 14:43:41', '2015-01-06 23:00:00'),
+(2, '/images/ads/iSquid_______iPod_parody_ad_by_ashbet.jpg', 'http://dev.mihx.es', 0, 3080, '2013-12-20 14:44:25', '2015-12-29 23:00:00'),
+(3, 'http://www.ecoeastend.com/images/ad%20images/half-ad-1.gif', 'http://dev.mihx.es', 0, 3344, '2013-12-17 23:00:00', '2015-12-18 23:00:00'),
+(4, 'http://farm5.staticflickr.com/4078/4743777048_e8ac9c0c8a.jpg', 'http://dev.mihx.es', 0, 3074, '2013-12-19 23:00:00', '2015-01-09 23:00:00');
 
 -- --------------------------------------------------------
 
@@ -118,18 +118,24 @@ CREATE TABLE `item` (
   PRIMARY KEY (`id`),
   KEY `user` (`user_id`),
   KEY `category` (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 --
 -- Volcado de datos para la tabla `item`
 --
 
 INSERT INTO `item` (`id`, `user_id`, `category_id`, `title`, `description`, `price`, `phone`, `image_url`, `location`, `date_published`, `date_end`, `num_views`, `premium`) VALUES
-(2, 1, 2, 'titulo1', 'Anuncio 2, negocios', 22450, 666123456, '/img/placeholder.png', 'Barcelona', '2014-01-07 15:39:28', '2015-01-08 15:39:28', 0, 0),
-(3, 2, 2, 'title1', 'Anuncio 3, trabajo', 13072, 666123456, '/img/placeholder.png', 'Barcelona', '2014-02-25 15:39:31', '2015-03-04 15:39:31', 0, 1),
-(5, 2, 3, 'Title a bit longer', 'Description', 432, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-25 16:03:18', '2015-03-02 16:03:18', 0, 0),
-(6, 2, 7, 'Titulo uno', 'Texto texto texto', 1982, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-25 17:52:15', '2015-02-28 17:52:15', 0, 1),
-(7, 2, 1, '中国 苏州 三香路999号707室', '(西班牙独资) 苏州西利克贸易有限公司', 2000, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-26 15:01:45', '2015-03-04 15:01:45', 0, 0);
+(2, 1, 2, 'titulo1', 'Anuncio 2, negocios', 22450, 666123456, '/img/placeholder.png', 'Barcelona', '2014-01-07 15:39:28', '2015-01-08 15:39:28', 8, 1),
+(3, 2, 3, 'title1', 'Anuncio 3, trabajo', 13072, 666123456, '/img/placeholder.png', 'Barcelona', '2014-02-25 15:39:31', '2015-03-04 15:39:31', 0, 1),
+(5, 2, 3, 'Title a bit longer', 'Description, alquiler', 432, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-25 16:03:18', '2015-03-02 16:03:18', 0, 1),
+(6, 2, 7, 'Titulo uno', 'Texto texto texto, rebajas!', 1982, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-25 17:52:15', '2015-02-28 17:52:15', 0, 1),
+(7, 2, 1, '中国 苏州 三香路999号707室', '(西班牙独资) 苏州西利克贸易有限公司', 2000, 666666666, '/img/placeholder.png', 'Barcelona', '2014-02-26 15:01:45', '2015-03-04 15:01:45', 5, 1),
+(8, 2, 3, '中国 苏州 三香路999号707室', '(西班牙独资) 苏州西利克贸易有限公司', 2200, 666666666, '/img/placeholder.png', 'Madrid', '2014-02-26 15:01:45', '2015-03-04 15:01:45', 0, 0),
+(16, 1, 3, '123123123123', 'Experiencia de 3 a?os', 2000, 666666666, '/images/uploads/Icon~ipad.png', 'Barcelona', '2014-04-02 11:08:07', '2015-04-03 11:08:07', 0, 0),
+(17, 1, 3, 'title1, test', 'una habitación por 200&euro;', 200, 666666666, '/images/uploads/JI004.png', 'Barcelona', '2014-04-02 11:19:07', '2015-04-03 11:19:07', 18, 1),
+(18, 1, 3, 'test', 'test', 123, 666666666, '/img/placeholder.png', 'Barcelona', '2014-04-11 14:51:55', '2014-06-10 14:51:55', 2, 1),
+(19, 1, 3, 'test', 'test', 123, 666666666, '/img/placeholder.png', 'Barcelona', '2014-04-11 14:53:08', '2014-06-10 14:53:08', 1, 0),
+(20, 1, 6, 'test', 'Texto texto texto', 123, 666666666, '/img/placeholder.png', 'Barcelona', '2014-04-11 14:53:37', '2014-06-10 14:53:37', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -168,6 +174,7 @@ CREATE TABLE `purchase` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `method` enum('BANK_TRANSFER','PAYPAL','CREDIT_CARD') NOT NULL,
+  `price` float NOT NULL,
   `num_credits` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` tinyint(1) NOT NULL DEFAULT '0',
@@ -175,22 +182,7 @@ CREATE TABLE `purchase` (
   `payment_token` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
-
---
--- Volcado de datos para la tabla `purchase`
---
-
-INSERT INTO `purchase` (`id`, `user_id`, `method`, `num_credits`, `date`, `status`, `token`, `payment_token`) VALUES
-(1, 1, 'BANK_TRANSFER', 200, '2013-12-20 09:13:32', 0, '123asd', ''),
-(12, 2, 'PAYPAL', 1, '2014-02-24 11:58:00', 0, 'EC-0K993033JW576483E', 'PAY-24M66432NW790614JKMFTHRY'),
-(13, 2, 'PAYPAL', 1, '2014-02-24 12:03:09', 0, 'EC-3RT74001UP758041J', 'PAY-8SN85955U86716200KMFTJ7A'),
-(14, 2, 'PAYPAL', 1, '2014-02-24 12:40:41', 1, 'EC-3KV52251JE7186832', 'PAY-3TH55781NC4590607KMFT3RY'),
-(15, 2, 'PAYPAL', 1, '2014-02-24 12:44:59', 1, 'EC-9CW43057Y3509364R', 'PAY-0J4566536H3201848KMFT5SQ'),
-(16, 2, 'PAYPAL', 5, '2014-02-24 12:47:17', 1, 'EC-66392315YR279264J', 'PAY-0YS515898Y913874NKMFT6UY'),
-(17, 2, 'PAYPAL', 5, '2014-02-24 12:48:06', 1, 'EC-0SG9245861179942E', 'PAY-4VN088655W176833TKMFT7BI'),
-(18, 2, 'PAYPAL', 1, '2014-02-24 12:49:49', 0, 'EC-4KT09292B4388824G', 'PAY-5C259728WR5183936KMFT73A'),
-(19, 2, 'PAYPAL', 25, '2014-02-25 11:29:44', 1, 'EC-7Y344755SA9887942', 'PAY-9WD76503PW9310425KMGH5JI');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -212,15 +204,17 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`id`, `active`, `phone`, `email`, `password`, `credits`, `date_register`, `token`, `device_id`, `push_id`) VALUES
-(1, 1, NULL, 'admin', '$2a$13$DAKriMormJCmK.rU3UmeY.4FJu5CjkNYVErs9cIe3KntIZgTctQBC', 10000000, '2013-12-19 17:38:07', '', '', ''),
-(2, 1, 666666666, 'test@test.com', '$2a$13$duhqzJpn1rKcNev6LMQXL.sZqTeYATYtcILZ7gFLLhCoBzczzWpBW', 17, '2014-02-12 17:10:20', 'oLTq1sQCrp3PYlgoqgjJHCi8B', NULL, NULL);
+(1, 1, NULL, 'admin', '$2a$13$DAKriMormJCmK.rU3UmeY.4FJu5CjkNYVErs9cIe3KntIZgTctQBC', 160, '2013-12-19 17:38:07', '', '', ''),
+(2, 1, 666666666, 'friko1@gmail.com', '$2a$13$duhqzJpn1rKcNev6LMQXL.sZqTeYATYtcILZ7gFLLhCoBzczzWpBW', 17, '2014-02-12 17:10:20', 'Mdhb02EpG5aK8M2HIy2lqgPSv', NULL, NULL),
+(4, 0, 666666172, 'lalala@lalala.com', '$2a$13$CZ9lTX.MNqIivfRxpE0z2uLpv52FDsRnlspPpxfKyEjTJL9fuhPQa', 0, '2014-03-27 16:41:46', '4Y2RjOOZyx42qnO0UcRAmzP1N', NULL, NULL),
+(5, 1, 645155625, 'friko67@gmail.com', '$2a$13$Y5zkrbpfQh/tJLCQxORRb.r0MRpp7VQWXZdQYt6Q9KnYAsmZBwljq', 1, '2014-04-08 13:47:05', 'lbdUTHjba6WjZxB4e4p27OhF7', NULL, NULL);
 
 --
 -- Restricciones para tablas volcadas

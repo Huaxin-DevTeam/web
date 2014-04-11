bdd63fdab148c63a87f4e6c340d6dbf98dd6b1b5
iris <iris.serranoc@gmail.com>
2014-04-11 18:24:49 +0200

    tabla

--- protected/views/order/select.php
+++ protected/views/order/select.php
@@ -8,8 +8,8 @@ foreach($options as $opt)
 </ul>
 
 
-<table style="text-align:center; width: 100%;">
-<tr><td colspan="3"><h5><?php print Yii::t("huaxin","Tabla de costes"); ?></h5></td></tr>
+<table style="width: 100%;" class="table">
+<tr><th colspan="3"><?php print Yii::t("huaxin","Tabla de costes"); ?></th></tr>
 <tr><td>1 semana: 3 créditos</td><td>1 mes: 9 créditos</td><td>1 año: 30 créditos</td></tr>
 <tr><td>2 semanas: 5 créditos</td><td>2 meses: 15 créditos</td><td><b>Destacar anuncio en portada: Tarifa x3</b></td></tr>
 </table>

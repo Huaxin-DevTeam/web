463f05efd6e5eb76a5ed1760b53bc5ac1678ef25
iris <iris.serranoc@gmail.com>
2014-05-26 19:36:24 +0200

    hello

--- protected/views/mail/confirmation.php
+++ protected/views/mail/confirmation.php
@@ -1,4 +1,4 @@
-<p><?php print Yii::t("huaxin","Hello")  $email ?>,</p>
+<p><?php print Yii::t("huaxin","Hello") .", ". $email ?>,</p>
 
 <p><?php print Yii::t("huaxin","Welcome to HUAXIN. Before you can log in and publish some ads, you must confirm your email.")?><br>
 	<?php print Yii::t("huaxin","To do so, please click on this link:")?> <a href="<?php echo $link ?>"><?php echo $link ?></a> <?php print Yii::t("huaxin","or copy and paste it on your browser.")?>
